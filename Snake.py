3aa182f387b9783da3fb972c78472b00d3acad97
Shahzadi Eman <147303808+Emanism@users.noreply.github.com>
2023-11-21 14:09:53 +0500

    Add files via upload

---
+++ Snake.py
@@ -0,0 +1,120 @@
+import pygame
+import sys
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 640, 480
+GRID_SIZE = 20
+FPS = 10
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# Snake class
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.positions = [((WIDTH // 2), (HEIGHT // 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+        self.color = GREEN
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def update(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0] + (x * GRID_SIZE)) % WIDTH), (cur[1] + (y * GRID_SIZE)) % HEIGHT)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+    def reset(self):
+        self.length = 1
+        self.positions = [((WIDTH // 2), (HEIGHT // 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
+    def render(self, surface):
+        for p in self.positions:
+            pygame.draw.rect(surface, self.color, (p[0], p[1], GRID_SIZE, GRID_SIZE))
+
+
+# Fruit class
+class Fruit:
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = RED
+        self.randomize_position()
+
+    def randomize_position(self):
+        self.position = (random.randint(0, (WIDTH // GRID_SIZE) - 1) * GRID_SIZE,
+                         random.randint(0, (HEIGHT // GRID_SIZE) - 1) * GRID_SIZE)
+
+    def render(self, surface):
+        pygame.draw.rect(surface, self.color, (self.position[0], self.position[1], GRID_SIZE, GRID_SIZE))
+
+
+# Directions
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+def draw_grid(surface):
+    for y in range(0, HEIGHT, GRID_SIZE):
+        for x in range(0, WIDTH, GRID_SIZE):
+            rect = pygame.Rect(x, y, GRID_SIZE, GRID_SIZE)
+            pygame.draw.rect(surface, WHITE, rect, 1)
+
+def main():
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+
+    snake = Snake()
+    fruit = Fruit()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    if snake.direction != DOWN:
+                        snake.direction = UP
+                elif event.key == pygame.K_DOWN:
+                    if snake.direction != UP:
+                        snake.direction = DOWN
+                elif event.key == pygame.K_LEFT:
+                    if snake.direction != RIGHT:
+                        snake.direction = LEFT
+                elif event.key == pygame.K_RIGHT:
+                    if snake.direction != LEFT:
+                        snake.direction = RIGHT
+
+        snake.update()
+        if snake.get_head_position() == fruit.position:
+            snake.length += 1
+            fruit.randomize_position()
+
+        surface.fill((0, 0, 0))
+        draw_grid(surface)
+        snake.render(surface)
+        fruit.render(surface)
+        screen.blit(surface, (0, 0))
+        pygame.display.update()
+        clock.tick(FPS)
+
+if __name__ == "__main__":
+    main()
+
